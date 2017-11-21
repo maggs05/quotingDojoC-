68db31f9ca120597f40473095efa08fe6c8d8ddf
maggs05 <maggs.gose05@hotmail.com>
2017-11-21 13:11:59 -0800

    Almost there... working on getting my quotes page to come up. made some changes and now it is not working.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -2,27 +2,33 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using DbConnection;
 
 namespace QuotingDojo1.Controllers{
     public class HomeController: Controller{
         [HttpGet]
         [Route("")]
         public IActionResult Index(){
-            HttpContext.Session.SetString("Name", "name");
-            HttpContext.Session.SetString("Quote","quote");
+            if(TempData["Error"]!=null){
+                ViewBag.Error=TempData["Error"];
+            }
             return View();
         }
         [HttpPost]
-        [Route("Quotes")]
-        public IActionResult Quotes(){
-            string nameVariable = HttpContext.Session.GetString("Name");
-            string quoteVariable = HttpContext.Session.GetString("Quote");
-            return View("Quotes");
+        [Route("/Quotes")]
+        public IActionResult Create(string name, string quote){
+            if(name ==""||quote==""){
+                TempData["Error"]="Both fields must be completed!";
+                return RedirectToAction("Index");
+            }
+            string query =$"INSERT INTO quotes (quote, name, created_at, updated_at) VALUES ('{quote}','{name}',NOW(),NOW());";
+            DbConnector.Execute(query);           
+            return RedirectToAction("Quotes");
             
         }
         [HttpGet]
-        [Route("Quotes")]
-        public IActionResult Skip(){
+        [Route("/Quotes")]
+        public IActionResult Quotes(){
             return View("Quotes");
 
         }
